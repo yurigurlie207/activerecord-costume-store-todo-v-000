977ffe9192b8b8e87af88086d1582daced59e527
Adrienne Yao <adrienne.yao@icloud.com>
2019-08-04 01:43:31 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,14 +10,14 @@
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
 
-def change
-  create_table :costumes do |t|
-   t.string :name
-   t.float :price
-   t.string :size
-   t.string :image_url
-   t.timestamps null: false
+  def change
+    create_table :costumes do |t|
+     t.string :name
+     t.float :price
+     t.string :size
+     t.string :image_url
+     t.timestamps null: false
+    end
   end
-end
 
 end
