0aa0578a4497b26bc1de5b4c9ebf8f8c77c25b7f
Adrienne Yao <adrienne.yao@icloud.com>
2019-08-04 01:34:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -21,7 +21,7 @@ def change
   create_table :costumes do |t|
    t.string :name
    t.float :price
-   t.integer :size
+   t.string :size
    t.string :image_url
    t.datetime :created_at
    t.datetime :updated_at
