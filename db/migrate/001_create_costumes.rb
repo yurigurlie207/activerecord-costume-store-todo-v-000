b52973e27bc03d84df6e787c8417c566fe1704bb
Adrienne Yao <adrienne.yao@icloud.com>
2019-08-04 01:34:14 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -20,7 +20,7 @@ class CreateArtists < ActiveRecord::Migration[4.2]
 def change
   create_table :costumes do |t|
    t.string :name
-   t.string :price
+   t.float :price
    t.integer :size
    t.string :image_url
    t.datetime :created_at
